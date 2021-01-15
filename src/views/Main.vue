053915aeaf30966c04d2800e540b374d0c570ff4
Tester <TestTestTest1000@Test.com.ru>
2021-01-15 17:29:29 +0300

    add some files and starting to create TheHeader.vue

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="home"></div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <app-filter></app-filter>
+        <app-table></app-table>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import Filter from '../components/Filter';
+import Table from '../components/Table';
+
 export default {
   name: 'Main',
   components: {
-    HelloWorld,
+    AppFilter: Filter,
+    AppTable: Table,
   },
 };
 </script>
