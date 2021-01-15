d3fde80eb845696a324b5c0b1c636cc10c82f4e5
Tester <TestTestTest1000@Test.com.ru>
2021-01-15 16:52:01 +0300

    change TheFooter.vue

---
+++ src/components/common/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        wbx-table-client 2021
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
